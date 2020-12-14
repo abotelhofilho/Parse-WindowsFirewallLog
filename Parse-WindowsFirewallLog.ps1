da85b94bdbc5ba3eeb7dc0a84d2efc89db905264
Antonino BotelhoFilho <abotelhofilho@users.noreply.github.com>
2020-12-14 16:32:27 -0500

    added default path.
    wanted to add default path for a while.  this will make my life easier.

--- Parse-WindowsFirewallLog.ps1
+++ Parse-WindowsFirewallLog.ps1
@@ -1,9 +1,14 @@
+<#
+Parses Windows firewall log into a managable objects where better filtering can be done
+
+default path is to windows default firewall log location.  If your file is located somewhere else use the -path parameter
+#>
+
 function Parse-WindowsFirewallLog
 {
     Param
     (
-         [Parameter(Mandatory=$True, Position=0)]
-         [string] $Path
+         [Parameter(Mandatory=$false)] [string] $Path = "C:\Windows\System32\LogFiles\Firewall\pfirewall.log"
     )
 ## Import file and use whitespace as the delimiter, set headers and skip the first three lines because windows puts in some junk in the first three lines for the logs files... enjoy
     Import-CSV -path $Path -Delimiter "` " -h Date , Time , Action , Protocol , Src_ip , Dst_ip , Src_port , Dst_port , Size , TCPflags , TCPsyn , TCPack , TCPwin , ICMPtype , ICMPcode , Info , Path | Select-Object -Skip 3
