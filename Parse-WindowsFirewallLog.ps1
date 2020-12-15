8e88f3c5f8e218de9f70979415e1a1802a6ac67a
Antonino BotelhoFilho <abotelhofilho@users.noreply.github.com>
2020-12-15 10:27:03 -0500

    implemented try catch for more validation
    implemented try catch for additional path validation.

--- Parse-WindowsFirewallLog.ps1
+++ Parse-WindowsFirewallLog.ps1
@@ -1,7 +1,7 @@
 <#
 Parses Windows firewall log into a managable objects for better filtering.  Best results is to pipe it into Format-Table.
 
-default path is to windows default firewall log location.  If your file is located somewhere else use the -path parameter.
+Default path is to windows default firewall log location.  If your file is located somewhere else use the -path parameter.
 #>
 
 function Parse-WindowsFirewallLog {
@@ -9,12 +9,19 @@ function Parse-WindowsFirewallLog {
     (
         [Parameter(Mandatory = $false)] [string] $Path = "C:\Windows\System32\LogFiles\Firewall\pfirewall.log"
     )
-    ## Import file and use whitespace as the delimiter, set headers and skip the first three lines because windows puts in some junk in the first three lines for the logs files... enjoy
-    If ( (Test-Path -Path $path ) -eq $true ) {
-        Import-CSV -path $Path -Delimiter "` " -h Date , Time , Action , Protocol , Src_ip , Dst_ip , Src_port , Dst_port , Size , TCPflags , TCPsyn , TCPack , TCPwin , ICMPtype , ICMPcode , Info , Path | Select-Object -Skip 3
-    }
-    else {
-        Write-Host "Error: Windows firewall logs are not located in the default location $path.  Please rerun providing the log path using the -path parameter."
-    }
+    try {
+        ## Test path to validate path parameter value.    
+        If ( (Test-Path -Path $path ) -eq $true ) {
+            ## Import file and use whitespace as the delimiter, set headers and skip the first three lines because windows puts in some junk in the first three lines for the logs files... enjoy
+            Import-CSV -path $Path -Delimiter "` " -h Date , Time , Action , Protocol , Src_ip , Dst_ip , Src_port , Dst_port , Size , TCPflags , TCPsyn , TCPack , TCPwin , ICMPtype , ICMPcode , Info , Path | Select-Object -Skip 3
+        }
+        else {
+            Write-Host "Error: Windows firewall logs are not located in the default location $path.  Please rerun providing the log path using the -path parameter.`n" -ForegroundColor Red
+        }
 
+    }
+    catch {
+        if ($error[0] -match "Could not find a part of the path") {Write-Host "Error: Windows firewall logs are not located in provided path $path.  Please rerun providing a correct the log path.`n" -ForegroundColor Red}
+    }
+    
 }
